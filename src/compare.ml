74e39c0c57b3849e87ef018b12cd791ff400a236
MrBigoudi <yannis.kedadry2@gmail.com>
2022-06-21 16:17:03 +0200

    init image to tree

--- src/compare.ml
+++ src/compare.ml
@@ -1,35 +1,118 @@
 open Vg;;
 open Gg;;
 
-module ManipulateVg : sig
-  (*Split string using ' ' as delimiter + remove empty component in the result array*)
-  val split : string -> string list
+type tr_token =
+    | Move of (float*float)
+    | Rot of float
+    | Scale of (float*float);;
 
+
+  type i_token = 
+    | Tr of tr_token
+    | Blend
+    | Cut
+    | Path of (float*float) list
+    | Outline of (float*float)
+    | Const of (float*float*float*float);;
+
+
+  type i_tree = 
+    | F of i_token
+    | Node of i_token * i_tree list;;
+
+module ManipulateVg : sig
+  (*Get offset of next left parenthesis*)
+  val next_left_p : string -> int -> int
+  (*Get offset of next right parenthesis*)
+  val next_right_p : string -> int -> int
+
+
+  (*Get a tr_token and the new offset*)
+  val get_tr_token : string -> int -> tr_token*int
+  (*Get the 2 new offsets to divide the next research*)
+  val get_blend_token : string -> int -> int*int
+  (*Get the list of new offsets to divide the next researches*)
+  val get_cut_token : string -> int -> int list
+  (*Get a const_token, ie (float float float float)*)
+  val get_const_token : string -> int -> (float*float*float*float)
+  (*Get a path_token, ie S (float float) L (float float) ... L (float float) Z*)
+  val get_path_token : string -> int -> (float*float) list
+  (*Get an outline_token, ie (width float) and (miter-angle float)*)
+  val get_outline_token : string -> int -> (float*float)
+  (*Split string using ' ' as delimiter + remove empty component in the result array*)
+
+
+  val split : string -> string list
   (*Filter list to omly keep numbers*)
   val empty_array : string list -> string list
-
   (*Remove parentheses in a list of strings*)
   val clean_array : string list -> string list
-
   (*Cast a string to a float*)
   val float_of_string : string -> float
-
   (*Debug tool to print a string list*)
   val print_arr : string list -> unit
-
   (*Cast a string list to a tuple of float list*)
   val create_tuple_list : string list -> (float * float) list
-
   (*Transform a Vg image to a tuple of float list*)
   val decompose : image -> (float * float) list
-
   (*Remove copies in a tuple of float list*)
   val remove_double : (float * float) list -> (float * float) list
-
   (*Compare two tuple of float list*)
   val compare_list_tuples : (float * float) list -> (float * float) list -> bool
 
 end = struct 
+  let next_left_p str n = 
+    String.index_from str n '(';;
+  
+
+  let next_right_p str n = 
+    String.index_from str n ')';;
+  
+
+  let get_tr_token str offset = failwith "TODO";;
+  let get_blend_token str offset = failwith "TODO";;
+  let get_cut_token str offset = failwith "TODO";;
+  let get_const_token str offset = failwith "TODO";;
+  let get_path_token str offset = failwith "TODO";;
+  let get_outline_token str offset = failwith "TODO";;
+
+
+  let create_i_tree str =
+    let len = String.length str in
+    let rec aux offset fin = 
+      if offset >= fin then []
+      else match str.[offset] with
+        (*i-tr*)
+        | 't' -> let (tr,new_offset) = (get_tr_token str offset)
+                  in [(Node(Tr(tr),(aux new_offset fin)))]
+        (*i-blend*)
+        | 'b' -> let (new_offset1,new_offset2) = (get_blend_token str offset)
+                  in [(Node(Blend,(aux new_offset1 new_offset2)@(aux new_offset2 fin)))]
+        (*i-const || i-cut*)
+        | 'c' -> let offset = offset+1 
+                  (*i-const*)
+                  in if str.[offset]=='o' then 
+                    let const = (get_const_token str offset)
+                    in [F(const)]
+                  (*i-cut*)
+                  else let new_offsets = (get_cut_token str offset) in
+                    let rec aux_cut l acc = 
+                      match l with
+                      | [] -> (Node(Cut,acc))
+                      | h::[] -> (aux_cut [] ((aux h fin)@acc))
+                      | h1::h2::t -> (aux_cut (h2::t) ((aux h1 h2)@acc))
+                    in [(aux_cut new_offsets [])]
+        (*path*)
+        | 'p' -> let path = (get_path_token str offset)
+                    in [F(path)]
+        (*outline*)
+        | 'o' -> let outline = (get_outline_token str offset)
+                    in [F(outline)] 
+        (*error*)
+        | _ -> let res = Printf.sprintf "unknown token at pos: %d\n" offset 
+                    in failwith res
+    in (aux 3 len);;
+
 
   let split str =
     str
@@ -48,6 +131,7 @@ end = struct
 
   let clean_array arr = 
     let f s = 
+      (*remove right parentheses*)
       let rec f1 s = 
         let len = (String.length s) in
         match s.[len-1] with
@@ -55,12 +139,14 @@ end = struct
         | ')' -> f1 (String.sub s 0 (len-1))
         | _ -> s 
       in 
+      (*remove left parentheses*)
       let rec f2 s = 
         let len = (String.length s) in
         match s.[0] with
         | '(' -> f2 (String.sub s 1 (len-1)) 
         | _ -> s
       in (f2 (f1 s))
+    (*remove all parentheses*)
     in (List.map f arr);;
 
   let float_of_string s =
@@ -102,7 +188,62 @@ end = struct
 
 end
 
-let image_equal i1 i2 =
-  let i1_tuples = ManipulateVg.decompose i1 in
-  let i2_tuples = ManipulateVg.decompose i2 in 
-  (ManipulateVg.compare_list_tuples i1_tuples i2_tuples);;
+
+(* (i-tr (move (0.5 0))
+  (i-tr (rot 1.5708)
+   (i-blend Over
+    (i-blend Over
+     (i-cut anz
+      (path S (0.1 0.142) L (0.14 0.142) L (0.14 0.182) L (0.1 0.182) L
+       (0.1 0.142) Z)
+      (i-const (1 0 0 1)))
+     (i-cut
+      (outline (width 0.004) (cap Butt) (join Miter) (miter-angle 0.200713))
+      (path S (0.1 0.142) L (0.14 0.142) L (0.14 0.182) L (0.1 0.182) L
+       (0.1 0.142) Z)
+      (i-const (0.522522 0 0 1))))
+    (i-blend Over
+     (i-blend Over
+      (i-cut anz
+       (path S (0.142 0.1) L (0.182 0.1) L (0.182 0.14) L (0.142 0.14) L
+        (0.142 0.1) Z)
+       (i-const (1 0 0 1)))
+      (i-cut
+       (outline (width 0.004) (cap Butt) (join Miter) (miter-angle 0.200713))
+       (path S (0.142 0.1) L (0.182 0.1) L (0.182 0.14) L (0.142 0.14) L
+        (0.142 0.1) Z)
+       (i-const (0.522522 0 0 1))))
+     (i-blend Over
+      (i-blend Over
+       (i-cut anz
+        (path S (0.142 0.142) L (0.182 0.142) L (0.182 0.182) L (0.142 0.182)
+         L (0.142 0.142) Z)
+        (i-const (1 0 0 1)))
+       (i-cut
+        (outline (width 0.004) (cap Butt) (join Miter) (miter-angle 0.200713))
+        (path S (0.142 0.142) L (0.182 0.142) L (0.182 0.182) L (0.142 0.182)
+         L (0.142 0.142) Z)
+        (i-const (0.522522 0 0 1))))
+      (i-blend Over
+       (i-cut anz
+        (path S (0.184 0.1) L (0.224 0.1) L (0.224 0.14) L (0.184 0.14) L
+         (0.184 0.1) Z)
+        (i-const (1 0 0 1)))
+       (i-cut
+        (outline (width 0.004) (cap Butt) (join Miter) (miter-angle 0.200713))
+        (path S (0.184 0.1) L (0.224 0.1) L (0.224 0.14) L (0.184 0.14) L
+         (0.184 0.1) Z)
+        (i-const (0.522522 0 0 1))))))))) *)
+ 
+
+
+
+
+
+
+
+
+
+
+
+  
